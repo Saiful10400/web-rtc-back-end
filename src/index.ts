1915a93d5ca6bc64e075a405d15fd417b54b471c
saiful10400 <saifulislam10400i5@gmail.com>
2025-08-01 16:52:51 +0600

    project initialized.

---
+++ src/index.ts
@@ -0,0 +1,136 @@
+import express, { Request, Response } from "express";
+import cors from "cors"
+
+// socket io.
+import {createServer} from "http"
+import { Server } from "socket.io";
+import { Socket } from "dgram";
+
+const app=express()
+app.use(express.json())
+app.use(cors())
+
+const httpServer=createServer(app)
+const io=new Server(httpServer,{
+  cors: {
+    origin: [
+      "http://localhost:5173",
+    ],
+    methods: ["GET", "POST"],
+  },
+})
+
+type Tuser={
+  name:string,email:string,id:string
+}
+
+let user:Tuser[]=[]
+
+io.on("connection",(socket)=>{
+
+socket.on("join_user", (data) => {
+  if (!user.find((item) => item.id === socket.id)) {
+    user.push({ ...data, id: socket.id });
+    io.to(socket.id).emit("success_join",{success:true,email:data.email})
+ 
+  }
+});
+socket.on("disconnect", () => {
+  const id = user.find((item) => item.id === socket.id)?.id
+  user = user.filter((item) => item.id !== socket.id);
+});
+
+socket.on("get_active_user",()=>{
+  io.emit("active_user_list",user)
+  })
+
+
+socket.on("Incoming_call_send",(data)=>{
+  const receiver=user.find(item=>item.email===data.receiver)
+  const sender=user.find(item=>item.email===data.sender)
+  if(receiver){
+    io.to(receiver.id).emit("Incoming_call_receive",{offer:data.offer,receiver,sender})
+  }
+  })
+
+  //#negotiation.
+  socket.on("Incoming_call_send_negotiation",(data)=>{
+    const receiver=user.find(item=>item.email===data.receiver)
+    const sender=user.find(item=>item.email===data.sender)
+    if(receiver){
+      io.to(receiver.id).emit("Incoming_call_receive_negotiation",{offer:data.offer,receiver,sender})
+    }
+    })
+
+
+
+  
+socket.on("call_accepted",(data)=>{
+  const sender=user.find(item=>item.email===data.sender)
+  if(sender){
+    io.to(sender.id).emit("Receiver_call_received",{answer:data.answer,receiver:data.receiver,sender})
+  }
+})
+
+//# negotiation
+socket.on("call_accepted_negotiation",(data)=>{
+  const sender=user.find(item=>item.email===data.sender)
+  if(sender){
+    io.to(sender.id).emit("Receiver_call_received_negotiation",{answer:data.answer,receiver:data.receiver,sender})
+  }
+})
+
+
+
+// handle negotiation.
+
+
+
+
+
+})
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get("/",(req:Request,res:Response)=>{
+  res.send([
+      {
+        id: 1,
+        name: "John Doe",
+        age: 30,
+        email: "john.doe@example.com"
+      },
+      {
+        id: 2,
+        name: "Jane Smith",
+        age: 25,
+        email: "jane.smith@example.com"
+      },
+      {
+        id: 3,
+        name: "Sam Green",
+        age: 35,
+        email: "sam.green@example.com"
+      }
+    ])
+})
+
+
+httpServer.listen(5000,()=>{
+    console.log(`this server is runnning at http://localhost:5000`)
+})
